08d7cc79bf8d57bf66773d71dcd3c3798f3227d7
doubtfire009 <756241126@qq.com>
2018-11-26 22:23:58 +0800

    halcon

--- markpoints/test_marks_halcon.py
+++ markpoints/test_marks_halcon.py
@@ -15,25 +15,17 @@ def vertexFinder(xList,yList):
         xMin_y = [xMinIndex, yMinIndex]
         x_yMax = [xMinIndex, yMaxIndex]
         x_yMin = [xMaxIndex, yMinIndex]
-    # xMax_y = (xMaxIndex,yList[xList.index(xMaxIndex)])
-    # xMin_y = (xMinIndex,yList[xList.index(xMinIndex)])
-    # x_yMax = (xList[yList.index(yMaxIndex)],yMaxIndex)
-    # x_yMin = (xList[yList.index(yMinIndex)],yMinIndex)
 
-
-
-    print([xMax_y,xMin_y,x_yMax,x_yMin])
     return [xMax_y,xMin_y,x_yMax,x_yMin]
 
 threshold = 160
 img = cv2.imread("./img/halcon.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, th1 = cv2.threshold(gray, int(threshold), 255, cv2.THRESH_BINARY)
-# cv2.imwrite(settings.process_img_dir + "processed_gray.jpg", gray)
-# cv2.imwrite(settings.process_img_dir + "processed_th1.jpg", th1)
+
 image, contours, hierarchy = cv2.findContours(th1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(img,contours,-1,(0,0,255),3)
-# cv2.circle(img, (874, 1503), 100, (55, 255, 155), 30)
+
 key = 0
 xList = []
 yList = []
@@ -41,26 +33,19 @@ for element in contours:
     (x, y), radius = cv2.minEnclosingCircle(contours[key])
     xList.append(x)
     yList.append(y)
-    # cv2.circle(img, (int(x), int(y)), 3, (55, 255, 155), 2)  # 修改最后一个参数
     key = key + 1
-print(xList)
-print(yList)
+
 Vertexes = vertexFinder(xList,yList)
 
-# for i in Vertexes:
-#
-#     cv2.circle(img, (int(i[0]),int(i[1])), 3, (55, 255, 155), 2)  # 修改最后一个参数
+for i in Vertexes:
 
-    # print(Vertexes.index(i))
-cv2.circle(img, (60,320), 3, (55, 255, 155), 2)  # 修改最后一个参数
+    cv2.circle(img, (int(i[0]),int(i[1])), 3, (55, 255, 155), 2)  # 修改最后一个参数
+
+
 cv2.imshow("mark1", img)
 
 cv2.waitKey(0)
 
 
-def ListMaxFinder(list):
-    return list.index(max(list)),max(list)
 
-def ListMinFinder(list):
-    return list.index(min(list)),min(list)
 
